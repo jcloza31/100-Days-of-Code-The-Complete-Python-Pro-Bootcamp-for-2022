2a661726f04a662936683d1d2926937a6fa47b73
Joven Cloza <jcloza01@gmail.com>
2022-11-12 20:53:29 +0800

    Update main.py

--- day02/tip_calculator/main.py
+++ day02/tip_calculator/main.py
@@ -6,10 +6,8 @@ tip = int(input("What percentage tip would you like to  give? 10, 12, or 15? "))
 no_of_pax = int(input("How many people to split the bill? "))
 
 tip_percentage = (tip / 100) 
-
 total_bill = bill * (1 + tip_percentage)
-
-bill_per_pax = round(total_bill / no_of_pax, 2)
+bill_per_pax = "{:.2f}".format(total_bill / no_of_pax)
 
 result = f"Each person should pay: ${bill_per_pax}"
 print(result)
