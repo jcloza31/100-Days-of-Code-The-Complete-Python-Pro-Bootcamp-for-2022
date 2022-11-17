55e732601a7f887bc602e761b167aa64181bdbab
Joven Cloza <jcloza01@gmail.com>
2022-11-17 08:27:30 +0800

    Update main.py

--- day05/adding_even_number/main.py
+++ day05/adding_even_number/main.py
@@ -1,6 +1,5 @@
 #Write your code below this row 👇
 total = 0
-for number in range(1, 101):
-    if number % 2 == 0:
-        total += number
+for number in range(2, 101, 2):
+    total += number
 print(total)
