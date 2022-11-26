e9017d291c882cc5071978ff39bac931ce027092
Joven Cloza <jcloza01@gmail.com>
2022-11-26 20:01:21 +0800

    Update main.py

--- day07/hangman/main.py
+++ day07/hangman/main.py
@@ -1,9 +1,5 @@
-#Step 5
-
 import random
 
-#TODO-1: - Update the word list to use the 'word_list' from hangman_words.py
-#Delete this line: word_list = ["ardvark", "baboon", "camel"]
 from hangman_words import word_list
 
 chosen_word = random.choice(word_list)
@@ -12,7 +8,6 @@ word_length = len(chosen_word)
 end_of_game = False
 lives = 6
 
-#TODO-3: - Import the logo from hangman_art.py and print it at the start of the game.
 from hangman_art import logo
 print(logo)
 
