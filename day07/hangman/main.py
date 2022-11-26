48f3a87921ec1634640d8a046c07da4c491f6bad
Joven Cloza <jcloza01@gmail.com>
2022-11-26 20:02:03 +0800

    Update main.py

--- day07/hangman/main.py
+++ day07/hangman/main.py
@@ -17,8 +17,6 @@ for _ in range(word_length):
 
 while not end_of_game:
     guess = input("Guess a letter: ").lower()
-
-    #TODO-4: - If the user has entered a letter they've already guessed, print the letter and let them know.
     if guess in display:
         print(f"You've already guessed {guess}")
 
