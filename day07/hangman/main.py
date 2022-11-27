c1e43113f8bcda559d96ee99e699f3f9aedf8416
Joven Cloza <jcloza01@gmail.com>
2022-11-27 19:39:19 +0800

    Update main.py

--- day07/hangman/main.py
+++ day07/hangman/main.py
@@ -29,7 +29,6 @@ while not end_of_game:
 
     #Check if user is wrong.
     if guess not in chosen_word:
-        #TODO-5: - If the letter is not in the chosen_word, print out the letter and let them know it's not in the word.
         print(f"You guessed {guess}, that's not in the word. You lose a life.")
         
         lives -= 1
@@ -45,6 +44,5 @@ while not end_of_game:
         end_of_game = True
         print("You win.")
 
-    #TODO-2: - Import the stages from hangman_art.py and make this error go away.
     from hangman_art import stages
     print(stages[lives])
