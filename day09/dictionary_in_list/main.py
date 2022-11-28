c7a3f1644d88daa7ed9ad99c60d68783b3d9e70e
Joven Cloza <jcloza01@gmail.com>
2022-11-28 13:13:54 +0800

    Update main.py

--- day09/dictionary_in_list/main.py
+++ day09/dictionary_in_list/main.py
@@ -1,1 +1,25 @@
+travel_log = [
+{
+  "country": "France",
+  "visits": 12,
+  "cities": ["Paris", "Lille", "Dijon"]
+},
+{
+  "country": "Germany",
+  "visits": 5,
+  "cities": ["Berlin", "Hamburg", "Stuttgart"]
+},
+]
+#🚨 Do NOT change the code above
 
+#TODO: Write the function that will allow new countries
+#to be added to the travel_log. 👇
+
+def add_new_country(visited_country, times_of_visit, visited_cities):
+    travel_log.append({"country": visited_country, "visits": times_of_visit, "cities": visited_cities})
+
+
+
+#🚨 Do not change the code below
+add_new_country("Russia", 2, ["Moscow", "Saint Petersburg"])
+print(travel_log)
