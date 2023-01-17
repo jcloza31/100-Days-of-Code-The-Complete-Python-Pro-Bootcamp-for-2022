2d5e4230bd4fd1f7f1accf5a87a3a7116f24497d
Joven Cloza <jcloza01@gmail.com>
2023-01-17 09:17:41 +0800

    Update main.py

--- day12/number_guessing_game/main.py
+++ day12/number_guessing_game/main.py
@@ -1,4 +1,4 @@
 # The greatest glory in living lies not in never falling, but in rising every time we fall. – Nelson Mandela
 # “Never put off till tomorrow what may be done day after tomorrow just as well.” ― Mark Twain
 # “Only put off until tomorrow what you are willing to die having left undone” ― Pablo Picasso
-
+# “A year from now you may wish you had started today.” ― Karen Lamb
