7d223eb2e5695aa57c27c38b22309bdab4874600
Joven Cloza <jcloza01@gmail.com>
2023-01-18 10:34:06 +0800

    Update main.py

--- day12/number_guessing_game/main.py
+++ day12/number_guessing_game/main.py
@@ -2,3 +2,4 @@
 # “Never put off till tomorrow what may be done day after tomorrow just as well.” ― Mark Twain
 # “Only put off until tomorrow what you are willing to die having left undone” ― Pablo Picasso
 # “A year from now you may wish you had started today.” ― Karen Lamb
+# “You cannot escape the responsibility of tomorrow by evading it today.” ― Abraham Lincoln
